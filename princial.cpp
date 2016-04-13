0ae0f896b29852602e2a8c17dd7aba995a39a218
xxxxx <xxxx.@gmail.com>
2016-04-13 11:01:32 -0300

    Subtracao Adicionando funcao para fazer a subtracao.

--- princial.cpp
+++ princial.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 using namespace std;
 
+int subtracao();
+
 int main(int argc, char const *argv[])
 {
-    
-
+    int subtracaoValor = 0;
+    //Subtração
+        subtracaoValor = subtracao();
+        printf("O resultado é: %d", subtracaoValor);
         /* code */
     return 0;
 }
 
+int subtracao(){
+    int num1, int num2
+    printf("Digite um numero:");
+    scanf("%d", &num1);
+    printf("DIgite o numero que deseja subtrair do primeiro:");
+    scanf("%d", &num1);
+    int num3 = num1 - num2;
+    return num3;
+}
