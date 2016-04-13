8fed5a9796dbe56eb2306e222be65e7c26b7524f
jonnatas <jonatas_lenon@hotmail.com>
2016-04-13 11:02:18 -0300

    Finalizando Issue Soma

--- princial.cpp
+++ princial.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include "stdio.h"
+#include "stdlib.h"
+
 using namespace std;
+
+int Soma (){
+    int a;
+    int b;
+
+    printf("\nDigite o primeiro numero: ");
+    scanf ("%d",&a);
+
+  
+    printf("\nDigite o primeiro numero: ");
+    scanf("%d",&b);  
+  
+    int soma = a + b;
+  
+    return soma;
+}
+
 
 int main(int argc, char const *argv[])
 {
-    
+    int soma = Soma();
+    printf("\n%d\n",soma);
 
         /* code */
     return 0;
